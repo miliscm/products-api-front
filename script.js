789d86342ae99aa7538097ae0fc582b38dee60cc
Milena Mocci <miliscm@gmail.com>
2022-03-10 20:46:02 -0300

    refatorando o javascript

--- script.js
+++ script.js
@@ -1,32 +1,25 @@
 
-const url = "https://frontend-intern-challenge-api.iurykrieger.vercel.app/products"
+let url = "https://frontend-intern-challenge-api.iurykrieger.vercel.app/products?page="
+let pagesCount = 1;
 const cards = document.querySelector(".cards");
 const buttonMore = document.querySelector("#moreProducts")
 
-
-
 function createNode(element){
-  return  document.createElement(element)
+    return  document.createElement(element)
 }
+  
 
-
-    
 async function getProducts(){
-try{
-    const resp = await fetch(`${url}`);
+    try{
+
+    const resp = await fetch(`${url}${pagesCount}`);
     const data = await resp.json();
-    const productsList = data.products;  
-    const page = data.nextPage;
-   
-    if(productsList.length > 0){
-        return (productsList.map((item)=>{
-            // console.log(item)
-            let divNew = createNode("div");
-            divNew.classList.add("card");        
-            cards.insertAdjacentElement("afterbegin",divNew)
-            let itemCard = document.querySelector('.card')
-            // console.log(itemCard)
-            itemCard.innerHTML = `<div class="card-image">
+    const products = data.products;
+    const productId=document.getElementById('productsList')
+            
+     products.map((item)=>{       
+        let htmlContent = `<div class="card">
+        <div class="card-image">
             <img
               src="${item.image}"
               alt=""
@@ -39,22 +32,28 @@ try{
             </p>
             <p>De: R$${item.oldPrice}</p>
             <p>Por: R$${item.price}</p>
-            <button type="button">Comprar</button>`
-            
-        }))
-    }
+            <button type="button">Comprar</button>
+            </div>`
+                
+        productId.insertAdjacentHTML("beforeend",htmlContent)
 
-
-}
-catch(error){
-    console.log(error)
-}
+    }) 
+    if(pagesCount==2){
+        buttonMore.style.display="none";
+        
+    }  
     
+}
+    catch(error){
+    console.log("Erro na requisição")
+    }
 }
 
 getProducts();
 
-buttonMore.addEventListener("click",()=>
-{
-    getProducts()
-});
+buttonMore.addEventListener("click",()=>{
+    
+    pagesCount=pagesCount+1;
+    getProducts();
+}
+)
